a398a843a445c054b0882a27678dd90b049432cb
mrpeanutma <121702655+mrpeanutma@users.noreply.github.com>
2024-04-04 19:40:18 -0400

    messed with css for components

--- app/flashlite-components/Set.js
+++ app/flashlite-components/Set.js
@@ -4,14 +4,15 @@ import './Set.css';
 function Set(props) {
     return (    
         <li key={props.id} className="set-item">
-            <img src={props.img} className="set-img" alt={props.title} />
             <div className="set-info">
+                <img src={props.img} className="set-img" alt={props.title} />
                 <h2>{props.title}</h2>
-                <h3>{props.numOfTerms} Terms</h3>
-                <h3 className="user-content">Created by {props.creator}</h3>
             </div>
+                <div className="description">
+                    <h3>{props.numOfTerms} Terms</h3>
+                    <h3>Created by {props.creator}</h3>
+                </div>
         </li>
     );
 }
-
 export default Set;
--- app/page.js
+++ app/page.js
@@ -5,8 +5,9 @@ import {useState} from 'react';
 //import User from '/flashlite-components/User';
 //import UsersList from '/flashlite-components/UsersList';
 //import AddUser from './flashlite-components/AddUser';
-import Hdr from './/flashlite-components/Hdr';
-import AddSet from './/flashlite-components/AddSet';
+import Hdr from './flashlite-components/Hdr';
+import AddSet from './flashlite-components/AddSet';
+import SetList from './flashlite-components/SetList';
 
 function Home() {
   // const DEFAULT_IMAGE = "https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg";
@@ -46,6 +47,32 @@ function Home() {
   //     });
   //     console.log(users);
   // };
+
+  const DEFAULT_IMAGE = "https://www.pngall.com/wp-content/uploads/4/Flashlight-PNG-Clipart.png";
+
+  const DEFAULT_SETS = [
+    {
+      id: "e1",
+      img: DEFAULT_IMAGE,
+      title: "Web Programming Midterm Review",
+      numOfTerms: 37,
+      creator: "bailey_greer77"
+    },
+    {
+      id: "e2",
+      img: DEFAULT_IMAGE,
+      title: "History Test",
+      numOfTerms: 95,
+      creator: "mrpeanutma"
+    },
+    {
+      id: "e3",
+      img: DEFAULT_IMAGE,
+      title: "Biology Terms",
+      numOfTerms: 48,
+      creator: "jry39286"
+    }
+  ];
   
   const [signedIn, setSignedIn] = useState(); 
   
@@ -53,13 +80,17 @@ function Home() {
     return ( 
       <div> 
         <Hdr signedIn={true}/> 
-        <AddSet /> 
+        <p>Featured Sets</p>
+        <AddSet />
+        <SetList items={DEFAULT_SETS} /> 
       </div>
     )
   } else { 
     return ( 
       <div>         
-        <Hdr signedIn={false}/>        
+        <Hdr signedIn={false}/>
+        <p>Featured Sets</p>
+        <SetList items={DEFAULT_SETS} />
       </div>
     )
       
