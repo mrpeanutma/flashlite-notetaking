20c24dbb5ba79d892d3d1ceb0c50fa10ae6fdb57
Connor Stephens <connorwbstephens@gmail.com>
2024-04-07 13:56:21 -0400

    got rid of all commented code

--- app/page.js
+++ app/page.js
@@ -2,51 +2,11 @@
 
 import {useState} from 'react';
 
-//import User from '/flashlite-components/User';
-//import UsersList from '/flashlite-components/UsersList';
-//import AddUser from './flashlite-components/AddUser';
 import Hdr from './flashlite-components/Hdr';
 import AddSet from './flashlite-components/AddSet';
 import SetList from './flashlite-components/SetList';
 
 function Home() {
-  // const DEFAULT_IMAGE = "https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg";
-  
-  // const DEFAULT_USERS = [
-  //   {
-  //     id: Math.random().toString(),
-  //     name: "John Doe",
-  //     age: 20,
-  //     major: "Engineering",
-  //     img: DEFAULT_IMAGE,
-  //     //img: "https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg",
-  //   },
-  //   {
-  //     id: Math.random().toString(),
-  //     name: "Jane Doe",
-  //     age: 25,
-  //     major: "Marketing",
-  //     img: DEFAULT_IMAGE,
-  //     //img: "https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg",
-  //   },
-  //   {
-  //     id: Math.random().toString(),
-  //     name: "Chuck Noris",
-  //     age: 84,
-  //     major: "Acting",
-  //     img: DEFAULT_IMAGE,
-  //     //img: "https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg",
-  //   },
-  // ];
-
-  // const [users,setUsers] = useState(DEFAULT_USERS);
-
-  // const addUserHandler = (user) => {
-  //     setUsers ((prevUsers) => {
-  //         return [user, ...prevUsers];
-  //     });
-  //     console.log(users);
-  // };
 
   const DEFAULT_IMAGE = "https://www.pngall.com/wp-content/uploads/4/Flashlight-PNG-Clipart.png";
 
@@ -79,7 +39,7 @@ function Home() {
   if (signedIn) { 
     return ( 
       <div> 
-        <Hdr signedIn={true}/> 
+        <Hdr signedIn={true} /> 
         <p>Featured Sets</p>
         <AddSet />
         <SetList items={DEFAULT_SETS} /> 
@@ -88,7 +48,7 @@ function Home() {
   } else { 
     return ( 
       <div>         
-        <Hdr signedIn={false}/>
+        <Hdr signedIn={false} />
         <p>Featured Sets</p>
         <SetList items={DEFAULT_SETS} />
       </div>
@@ -96,7 +56,4 @@ function Home() {
       
   }
 }
-
-{/* <AddUser onAddUser={addUserHandler} />
-<UsersList items={users} /> */}
 export default Home;
