4179499559a18850db59c611dd18a89bcc07cc0b
Emily Cheng <ehc73013@uga.edu>
2024-04-24 19:54:05 -0400

    CAT GIPH

--- app/flashlite-components/Set.js
+++ app/flashlite-components/Set.js
@@ -4,8 +4,8 @@ import './css/Set.css';
 import Link from 'next/link';
 
 function Set(props) {
-    const DEFAULT_IMG = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXdBA9dnijNmKuHaYkEDKcy80J2Eexp4PEvMDCB0PcPg&s";
-
+    //const DEFAULT_IMG = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXdBA9dnijNmKuHaYkEDKcy80J2Eexp4PEvMDCB0PcPg&s";
+    const DEFAULT_IMG = "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExbjZzbHA4c25wdGFtYTZoMTJ2NzhkY3BndGFxeXRqZmVvZ2t6ODkxdyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/Ov5NiLVXT8JEc/200.webp"
     return (
         <li key={props.id} className="set-item">
             <Link href={`/set/${props.id}`} className='set-link'>
