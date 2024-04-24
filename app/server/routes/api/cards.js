5a6681096f9446f10aa69bcc8489a9985ad23aae
Connor Stephens <connorwbstephens@gmail.com>
2024-04-24 18:14:24 -0400

    moved new-card route to sets.js

--- app/server/routes/api/cards.js
+++ app/server/routes/api/cards.js
@@ -9,29 +9,6 @@ router.post('/', bodyParser.json(), (req, res) => {
         .catch((err) => res.status(400).json({error: 'Unable to add this set'}))
 });
 
-router.post('/:id/new-card', auth, async (req,res) => {
-    try {
-        const {term, def} = req.body;
-        if (!term || !def) {
-            return res.status(400).json({msg: 'Please enter all fields'});
-        }
-        
-        const newCard = new Card({term, def});
-        const savedCard = await newCard.save();
-        console.log(savedCard.term);
-
-        CardSet.findById(req.params.id)
-        .then((item) => {
-            item.cards.push(savedCard._id);
-            CardSet.findByIdAndUpdate(req.params.id, {"cards": item.cards});
-            res.json({item});
-        })
-        .catch((err) => res.status(404).json({ nosetfound: 'No set found'}))
-    } catch (err) {
-        res.status(500).json({error: err.message});
-    }
-});
-
 router.get('/:id', (req, res) => {
     Card.findById(req.params.id)
         .then((item) => res.json(item))
--- app/server/routes/api/sets.js
+++ app/server/routes/api/sets.js
@@ -11,17 +11,40 @@ router.post('/', bodyParser.json(), (req, res) => {
         .catch((err) => res.status(400).json({error: 'Unable to add this set'}))
 });
 
-router.post('/:id/new-card', bodyParser.json(), (req, res) => {
-    Card.create(req.body)
-    .then((item) => {
-        console.log(item);
-        CardSet.findByIdAndUpdate(req.params.id, 
-            {
-                $push: {cards: item._id}
-            })
+// router.post('/:id/new-card', bodyParser.json(), (req, res) => {
+//     Card.create(req.body)
+//     .then((item) => {
+//         console.log(item);
+//         CardSet.findByIdAndUpdate(req.params.id, 
+//             {
+//                 $push: {cards: item._id}
+//             })
+//         })
+//         .then((item) => res.json({ msg: 'Set added successfully'}))
+//         .catch((err) => res.status(400).json({err}))
+// });
+
+router.post('/:id/new-card', auth, async (req,res) => {
+    try {
+        const {term, def} = req.body;
+        if (!term || !def) {
+            return res.status(400).json({msg: 'Please enter all fields'});
+        }
+        
+        const newCard = new Card({term, def});
+        const savedCard = await newCard.save();
+        console.log(savedCard.term);
+
+        CardSet.findById(req.params.id)
+        .then((item) => {
+            item.cards.push(savedCard._id);
+            CardSet.findByIdAndUpdate(req.params.id, {"cards": item.cards});
+            res.json({item});
         })
-        .then((item) => res.json({ msg: 'Set added successfully'}))
-        .catch((err) => res.status(400).json({err}))
+        .catch((err) => res.status(404).json({ nosetfound: 'No set found'}))
+    } catch (err) {
+        res.status(500).json({error: err.message});
+    }
 });
 
 router.get('/:id', (req, res) => {
