1f23b4e64d6d0d1a6a263b7eb7223cce41057b67
Emily Cheng <ehc73013@uga.edu>
2024-04-24 11:35:59 -0400

    changed font to space Grotesk

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,9 @@
 import { Inter } from "next/font/google";
+import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
+const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
 
 export const metadata = {
   title: "FlashLITE",
@@ -14,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={spaceGrotesk.className}>{children}</body>
     </html>
   );
 }
