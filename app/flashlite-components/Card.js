722772fcd25ce434259c80b5b572c3e5b6c3fd77
jry39286 <jry39286@uga.edu>
2024-04-24 22:41:14 -0400

    /

--- app/flashlite-components/Card.js
+++ app/flashlite-components/Card.js
@@ -1,11 +1,18 @@
-
+import React from 'react';
+import './css/Card.css';
+import Link from 'next/link';
+import Button from './Button';
+import { useRouter } from "next/navigation";
 
 export default function Card(props) {
+    const router = useRouter();
     return (
         <li key={props.id} className="card-item">
-            {props.img ? (
+            {/* {props.img ? (
                 <img src={props.img} className="term-img" alt={props.title} />
-            ) : (<></>)}
+            ) : (<></>)} */}
+            <Button className="edit-button" onClick={() => {router.push('/edit-card')}}>Edit</Button>
+            <Button className="delete-button" onClick={() => {"Delete Item"}}>X</Button>
             <div className="term-info">
                 <div className="term">{props.term}</div>
                 <div className="definition">{props.definition}</div>
